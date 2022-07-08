5c1e24828eacdde1fe1341f1c06fbad66ec1ee5c
IgorCaenfa <101287264+IgorCaenfa@users.noreply.github.com>
2022-07-08 13:04:41 -0300

    Nodeshop-React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header/header';
+import Nav from './components/Nav/nav';
+import Main from './components/Main/main';
+import Section from './components/Section/section';
+import Footer from './components/Footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Section/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
